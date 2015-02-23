b43887336059cf536a5f248ad71217121a2b09dc
Nicolas Guerrero <nicg987@gmail.com>
2015-02-23 14:20:33 -0600

    act 1 stuff

--- ElevensLab/Activity1 Starter Code/CardTester.java
+++ ElevensLab/Activity1 Starter Code/CardTester.java
@@ -8,6 +8,32 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card[] cards = new Card[3];
+		cards[0] = new Card("K", "Clubs", 13);
+		cards[1] = new Card("K", "Spades", 13);
+		cards[2] = new Card("K", "Clubs", 13);
+		int j = 0;
+		
+		for (int i = 0; i < cards.length; i++)
+		{
+		    System.out.println(cards[i].suit());
+		    System.out.println(cards[i].rank());
+		    System.out.println(cards[i].pointValue());
+		    
+		    
+		    
+		    if (j == 2)
+		    {
+		        System.out.println("this card = first card? " + cards[j].matches(cards[0]) + "\n");
+		        
+		        j = 0; 
+		    }
+		    else
+		    {
+		        System.out.println("this card = next card? " + cards[j].matches(cards[j+1]) + "\n");
+		        
+		        j++;
+		    }
+		}
 	}
 }
