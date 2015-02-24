70cde8a814f6569b796168f6879223dcaf1457c8
Nicolas Guerrero <nicg987@gmail.com>
2015-02-24 15:10:12 -0600

    act2 Stuff

--- ElevensLab/Activity2  Starter Code/DeckTester.java
+++ ElevensLab/Activity2  Starter Code/DeckTester.java
@@ -3,11 +3,38 @@
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-	}
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *  @param args is not used.
+     */
+    public static void main(String[] args) {
+        Deck[] decks =  new Deck[3];
+        
+        String[][] ranks = new String[3][3];
+        ranks[0][0] = "A";
+        ranks[0][1] = "B";
+        ranks[0][2] = "C";
+        
+        String[][] suits = new String[3][2];
+        suits[0][0] = "Giraffes";
+        suits[0][1] = "Lions";
+        
+        int[][] values = new int[3][3];
+        values[0][0] = 2;
+        values[0][1] = 1;
+        values[0][2] = 6;
+        
+        
+        for (int i = 0; i < ranks.length; i++)
+        {
+            Deck deck = new Deck(ranks[i], suits[i], values[i]);
+            for (int j = 0; j < 7; j++)
+            {
+                System.out.println("Dealt: " + deck.deal());
+                System.out.println("Empty: " + deck.isEmpty());
+                System.out.println("Size: " + deck.size());
+            }
+            System.out.println("");
+        }
+    }
 }
