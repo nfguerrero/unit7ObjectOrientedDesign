1ea05de554979d994f7d4fd24e8e274b35bc9c81
Nicolas Guerrero <nicg987@gmail.com>
2015-02-26 15:09:46 -0600

    act3 stuffish

--- ElevensLab/Activity3 Starter Code/Shuffler.java
+++ ElevensLab/Activity3 Starter Code/Shuffler.java
@@ -51,12 +51,24 @@ public class Shuffler {
      */
     public static void perfectShuffle(int[] values) {
         int[] shuffled = new int[values.length];
-        int k = 0;
         
-        for (int j = 0; j < (values.length-values.length%2); j++)
+        int k = 0; 
+        for (int j = 0; j < (values.length/2+values.length%2); j++)
         {
-            shuffled
+            shuffled[k] = values[j];
+            k += 2;
         }    
+        k = 1;
+        for (int j = (values.length/2+values.length%2); j < values.length; j++)
+        {
+            shuffled[k] = values[j];
+            k += 2;
+        }
+        
+        for (int i = 0; i < shuffled.length; i++)
+        {
+            values[i] = shuffled[i];
+        }
     }
 
     /**
@@ -71,6 +83,6 @@ public class Shuffler {
      * @param values is an array of integers simulating cards to be shuffled.
      */
     public static void selectionShuffle(int[] values) {
-        /* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+        
     }
 }
